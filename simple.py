00678557cb38792cf143bcdfad31849de16059c5
Talya Kazayof <106309652+Talya2003@users.noreply.github.com>
2024-10-20 00:14:23 +0300

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,1 +1,43 @@
+# The motivation is:
+# print("the result iS:" , (2*3+4))
+# and the user will recieved : 10
 
+def addition(a, b) :
+    if a is None or b is None:
+        return None
+    return a+b
+
+def subtraction(a, b) :
+    if a is None or b is None:
+        return None
+    return a-b
+
+def multiplication(a, b) :
+    if a is None or b is None:
+        return None
+    return a*b
+
+def division(a, b) :
+    if a is None or b is None or b == 0:
+        print("Cannot dividion on 0")
+        return
+    return a/b
+
+
+def main() :
+    a = 7
+    b = 0
+    act = "/"
+    if act == "+":
+        print(addition(a,b))
+    elif act == "-":
+        print(subtraction(a, b))
+    elif act == "*":
+        print(multiplication(a, b))
+    elif act == "/":
+        print(division(a, b))
+    else:
+        print("Noooo")
+
+if __name__ == "__main__":
+    main()
